a707073c75def151d1883fbd74ea4862c6d71e7e
Engr Altaf <engraltaf@Mac-2.lan>
2025-08-23 23:20:57 +0400

    type annotations and inference and also types

---
+++ type_annotations_and_inference.ts
@@ -0,0 +1,28 @@
+const apples: number = 5;
+//apples = abc"
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+
+let myNumber: number[] = [1, 2, 3, 4, 5];
+
+let truths: boolean[] = [true, false, true];
+
+//Classes in the typescript
+class Car {}
+
+let car: Car = new Car();
+
+//Object literals
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
