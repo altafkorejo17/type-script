79f704a254fa8557e6414299a801bc97e5c05f93
Engr Altaf <engraltaf@Mac-2.lan>
2025-08-24 12:46:24 +0400

    tuples in the typescript

---
+++ tuples.ts
@@ -0,0 +1,18 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 30];
+//pepsi[0] = 40;
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  hrosepower: 400,
+  weight: 3354,
+};
